827ddb193689932469de84011a26b4b7ba9a362e
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-04-30 15:05:00 +0200

    add ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strncmp.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/31 14:56:56 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2020/11/28 10:14:26 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	st1;
+	unsigned char	st2;
+
+	i = 0;
+	st1 = (unsigned char)s1;
+	st2 = (unsigned char)s2;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+		}
+			else return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
