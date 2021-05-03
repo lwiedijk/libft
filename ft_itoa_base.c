636f9cdb3314c9187d0e2d19ac661c1072b4c832
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-05-03 16:28:21 +0200

    add itoa_base

---
+++ ft_itoa_base.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_itoa_base.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <lwiedijk@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/02/01 11:12:03 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2021/03/09 17:28:51 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_itoa_base(long long value, int base, char *hex_case)
+{
+	int		l;
+	int		remainder;
+	char	*output;
+
+	l = ft_numlen_base(value, base);
+	output = (char *)malloc(sizeof(char) * l + 1);
+	if (output == NULL)
+		return (NULL);
+	if (value == 0)
+		output[0] = '0';
+	if (value < 0 && base == 10)
+	{
+		output[0] = '-';
+		value = value * -1;
+	}
+	output[l] = '\0';
+	while (value)
+	{
+		l--;
+		remainder = value % base;
+		value = value / base;
+		output[l] = hex_case[remainder];
+	}
+	return (output);
+}
